aec6e69e0772fd95c09eb466f4ea796b465f0514
Jack Delahunty <jackd2012@hotmail.com>
2019-12-11 10:45:33 -0600

    stuff

---
+++ secretSanta/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  secretSanta
+//
+//  Created by period3 on 12/11/19.
+//  Copyright © 2019 period3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
---
+++ secretSantaTests/secretSantaTests.swift
@@ -0,0 +1,34 @@
+//
+//  secretSantaTests.swift
+//  secretSantaTests
+//
+//  Created by period3 on 12/11/19.
+//  Copyright © 2019 period3. All rights reserved.
+//
+
+import XCTest
+@testable import secretSanta
+
+class secretSantaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
